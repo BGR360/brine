b835c529a1225041369deaa46465a0e3c46dfec3
Ben Reeves <benwolverine2019@gmail.com>
2022-01-17 00:31:39 -0600

    brine_net: Re-export async_codec types

--- crates/brine_net/src/lib.rs
+++ crates/brine_net/src/lib.rs
@@ -93,6 +93,8 @@ mod system_param;
 
 pub mod codec;
 
+pub use async_codec::{Decode, DecodeResult, Encode, EncodeResult};
+
 pub use event::NetworkEvent;
 pub use plugin::{CodecReader, CodecWriter, NetworkPlugin};
 pub use resource::NetworkResource;
