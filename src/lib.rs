e6a2d5ac3d7ac975843ed9f1a2366611fcbae5c2
Ben Reeves <benwolverine2019@gmail.com>
2022-01-24 04:05:19 -0600

    I can show you the world!
    Main brine binary now lets you fly around the 10x10 chunk area sent by the server!

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,8 @@
+//! A Work-in-Progress Minecraft client written in Rust using the Bevy game engine.
+//!
+//! This library houses code that is common to the main Brine binary and other
+//! utility binaries in `src/bin/`.
+
 pub mod login;
+
+pub const DEFAULT_LOG_FILTER: &str = "wgpu_core=warn,naga=warn";
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,71 @@
+//! The Brine Minecraft client entrypoint.
+
 use bevy::{
-    log::{Level, LogPlugin, LogSettings},
+    log::{Level, LogSettings},
+    pbr::wireframe::{WireframeConfig, WireframePlugin},
     prelude::*,
+    render::{options::WgpuOptions, render_resource::WgpuFeatures},
 };
+use bevy_fly_camera::{FlyCamera, FlyCameraPlugin};
+use bevy_inspector_egui::prelude::*;
+
+use brine_proto::ProtocolPlugin;
+use brine_proto_backend::ProtocolBackendPlugin;
+use brine_voxel::chunk_builder::{ChunkBuilderPlugin, VisibleFacesChunkBuilder};
+
+use brine::{login::LoginPlugin, DEFAULT_LOG_FILTER};
 
 const SERVER: &str = "localhost:25565";
-const USERNAME: &str = "bgr360";
+const USERNAME: &str = "user";
 
 fn main() {
     App::new()
+        // Default plugins.
         .insert_resource(LogSettings {
             level: Level::INFO,
+            filter: String::from(DEFAULT_LOG_FILTER),
+        })
+        .add_plugins(DefaultPlugins)
+        // Debugging, diagnostics, and utility plugins.
+        // .add_plugin(WorldInspectorPlugin::new())
+        // Brine-specific plugins.
+        .add_plugin(ProtocolPlugin)
+        .add_plugin(ProtocolBackendPlugin)
+        .add_plugin(LoginPlugin::new(SERVER.to_string(), USERNAME.to_string()))
+        .add_plugin(MinecraftWorldViewerPlugin)
+        .run();
+}
+
+#[derive(Default)]
+pub struct MinecraftWorldViewerPlugin;
+
+impl Plugin for MinecraftWorldViewerPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(WgpuOptions {
+            features: WgpuFeatures::POLYGON_MODE_LINE,
             ..Default::default()
         })
-        // .add_plugins(MinimalPlugins)
-        .add_plugins(DefaultPlugins)
-        //.add_plugin(LogPlugin)
-        .add_plugin(brine_proto::ProtocolPlugin)
-        .add_plugin(brine_proto_backend::ProtocolBackendPlugin)
-        .add_plugin(brine::login::LoginPlugin::new(
-            SERVER.to_string(),
-            USERNAME.to_string(),
-        ))
-        .run();
+        .insert_resource(WireframeConfig { global: true })
+        .insert_resource(Msaa { samples: 4 })
+        .add_plugin(WireframePlugin)
+        .add_plugin(FlyCameraPlugin)
+        .add_plugin(ChunkBuilderPlugin::<VisibleFacesChunkBuilder>::default())
+        .add_startup_system(set_up_camera);
+    }
+}
+
+fn set_up_camera(mut commands: Commands) {
+    // Screenshot coords.
+    let camera_start = Transform::from_translation(Vec3::new(-200.0, 87.8, 157.3))
+        .with_rotation(Quat::from_euler(EulerRot::XYZ, 0.1338, 0.183, -0.025));
+
+    // let camera_start = Transform::from_translation(Vec3::new(-260.0, 115.0, 200.0))
+    //     .looking_at(Vec3::new(-40.0, 100.0, 0.0), Vec3::Y);
+
+    commands
+        .spawn_bundle(PerspectiveCameraBundle {
+            transform: camera_start,
+            ..Default::default()
+        })
+        .insert(FlyCamera::default());
 }
